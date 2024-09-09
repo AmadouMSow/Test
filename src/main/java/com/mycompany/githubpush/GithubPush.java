9cfb6d83df2c3b9604c8d76e60e934de32af3e8f
RC_Student_lab <RC_Student_lab@10.0.0.252>
2024-09-09 08:22:50 +0000

    -second commit (This added a scanner asking for name)

--- src/main/java/com/mycompany/githubpush/GithubPush.java
+++ src/main/java/com/mycompany/githubpush/GithubPush.java
@@ -3,12 +3,22 @@
  */
 
 package com.mycompany.githubpush;
+import java.util.Scanner;
 
 /**
  *
  * @author RC_Student_lab
  */
 public class GithubPush {
+    
+    public static void githubChange() {
+       //Import the scanner
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Please enter your name: ");
+       String name = scanner.next();
+        System.out.println("Hello " + name + " !.Welcome!");
+       
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
